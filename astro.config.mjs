0c08fd2bf4640554180f32895b5a217ee166a7b6
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-03-13 23:22:24 -0600

    A

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
+
+// https://astro.build/config
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tutorial.kasump.xyz",
   integrations: [preact()]
 });
